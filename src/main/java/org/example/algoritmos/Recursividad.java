1281e71dc0c45d2ba0e44a22f0e37e02632549c8
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-08-27 00:20:52 -0500

    class Recursividad con fibonacci

--- src/main/java/org/example/algoritmos/Recursividad.java
+++ src/main/java/org/example/algoritmos/Recursividad.java
@@ -1,2 +1,26 @@
-package org.example.algoritmos;public class Recursividad {
+package org.example.algoritmos;
+
+public class Recursividad {
+
+    // esta recursividad es muy ineficiente dado que su O(n) es de 2^n (exponencial), la mejor forma de hacerlo es con un bucle de crecimiento lineal
+    public int fibRecursivo(int n){
+        if (n == 0 || n == 1){
+            return n;
+        } else {
+            return fibRecursivo(n-1) + fibRecursivo(n-2);
+        }
+    }
+
+    // fibonacci con bucle, su crecimiento es lineal O(n). Este es el mejor método para hacerlo, no se usa recursividad
+    public int fibIterativo(int n){
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 0; i < n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
 }
