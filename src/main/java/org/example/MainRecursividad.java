131ff615e0565340ef440b6633d2390a7068fab1
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-08-27 00:22:00 -0500

    implementando recursividad fibonacci y su forma iterativa

---
+++ src/main/java/org/example/MainRecursividad.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.algoritmos.Recursividad;
+
+public class MainRecursividad {
+
+    public static void main(String[] args) {
+
+        Recursividad recursividad = new Recursividad();
+
+        System.out.println("Fibonacci Iterativo: "+recursividad.fibIterativo(7));
+        System.out.println("Fibonacci Recursivo: "+recursividad.fibRecursivo(7));
+    }
+}
