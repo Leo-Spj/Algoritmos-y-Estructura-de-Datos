eea766cf4e8a9acc22025f29fdb76afd868a21c0
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-08-31 10:07:27 -0500

    clase03-por seleccion

---
+++ src/main/java/org/example/algoritmos/clase03/Main_cOrdenamiento.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.example.algoritmos.clase03;
+
+/**
+ *
+ * @author lsilvau
+ */
+public class Main_cOrdenamiento {
+
+    public static void main(String[] args) {
+        int n=10;
+        cOrdenamiento oOrden=new cOrdenamiento(n);
+        oOrden.llenaArreglo();
+        System.out.println(oOrden.toString());
+        //oOrden.burbuja_Intercambio();
+        oOrden.insercion();
+        System.out.println(oOrden.toString());
+    }
+}
